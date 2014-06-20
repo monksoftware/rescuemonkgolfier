056265c53d011b5400d0153e079e24ea7b1a2ab2
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2014-06-20 15:41:12 +0200

    Add Socket.io

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var express = require("express");
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var bodyParser = require('body-parser');
 var path = require('path');
-
-var app = express();
 
 app.use(bodyParser());
 
@@ -10,6 +12,6 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var server = app.listen(3000, function() {
-  console.log('Listening on port %d', server.address().port);
+http.listen(3000, function() {
+  console.log('Listening on port %d', http.address().port);
 });
